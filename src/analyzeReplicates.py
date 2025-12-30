0a9d404a2f356bf2a41c61e21a824fcc160efea6
RoundBearChoi <roundbeargames@gmail.com>
2025-12-30 18:37:57 +0900

    save

--- src/analyzeReplicates.py
+++ src/analyzeReplicates.py
@@ -1,31 +1,14 @@
 # analyzeReplicates.py
 
-import os
 import pandas as pd
-import numpy as np
+from loadCSV import load_from_file as _load
 
 
 def analyze_reps(asset1: str, asset2: str) -> None:
     print('')
     print(' --- analyzing replicates --- ')
     
-    scriptDir = os.path.dirname(os.path.abspath(__file__))
-    dataDir = os.path.join(scriptDir, '..', 'data')
-    asset1 = asset1.lower()
-    asset2 = asset2.lower()
-
-    fileName = f'{asset1}_{asset2}_replicates_ordered.csv'
-    filePath = os.path.join(dataDir, fileName)
-
-    if not os.path.exists(filePath):
-        raise FileNotFoundError(f'file not found: {filePath}')
-
-    df = pd.read_csv(filePath)
-
-    expectedCols = ['replicate_index','date',f'{asset1}_{asset2}_price','change_pct']
-    missing = [col for col in expectedCols if col not in df.columns]
-    if missing:
-        raise ValueError(f'missing expected column: {missing}')
+    df = _load(f'{asset1}_{asset2}_replicates_ordered.csv', ['replicate_index', 'date', f'{asset1}_{asset2}_price', 'change_pct'])
 
     # get 0~5th percentile from each replicate_index and save to asset1_asset2_0_5_replicates_ordered.csv
 
