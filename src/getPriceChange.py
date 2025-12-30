0617ab7f9bfb38b0822d10cea22e03339334ca87
RoundBearChoi <roundbeargames@gmail.com>
2025-12-30 13:28:13 +0900

    save

--- src/getPriceChange.py
+++ src/getPriceChange.py
@@ -6,12 +6,12 @@ from saveCSV import save_to_file as _save
 
 
 def get_price_change(asset1: str, asset2: str) -> None:
-    print('')
-    print(f' --- calculating {asset1}_{asset2} price change --- ')
-
     asset1 = asset1.lower()
     asset2 = asset2.lower()
 
+    print('')
+    print(f' --- calculating {asset1}_{asset2} price change --- ')
+    
     df = _load(f'{asset1}_{asset2}_price.csv', ['date', asset1, asset2, 'ratio'])
 
     # calculate percentage change in the ratio from previous day
